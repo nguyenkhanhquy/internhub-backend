f49d2374bd5a79f51b35068732ffbe437b07eb0e
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-29 23:35:05 +0700

    add: custom exception

---
+++ src/main/java/com/internhub/backend/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.internhub.backend.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final EnumException enumException;
+
+    public CustomException(EnumException enumException) {
+        super(enumException.getMessage());
+        this.enumException = enumException;
+    }
+}
