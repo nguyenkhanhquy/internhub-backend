4b73d67f5f624fa3b095a5bbfe72dbb9b8db3fba
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-29 23:36:23 +0700

    add: error response

---
+++ src/main/java/com/internhub/backend/dto/response/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.internhub.backend.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+
+    @Builder.Default
+    private boolean success = false;
+
+    private int statusCode;
+
+    private String message;
+}
