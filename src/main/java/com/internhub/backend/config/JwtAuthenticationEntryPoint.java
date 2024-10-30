8a7967bb61eb2e7fb4f5c4e6158ab45f6baf69bd
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-30 11:59:06 +0700

    add: Jwt Authentication Entry Point

---
+++ src/main/java/com/internhub/backend/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package com.internhub.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.internhub.backend.dto.response.ErrorResponse;
+import com.internhub.backend.exception.EnumException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        EnumException enumException = EnumException.UNAUTHENTICATED;
+
+        response.setStatus(enumException.getStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(enumException.getMessage())
+                .statusCode(enumException.getStatusCode().value())
+                .build();
+
+        writeResponse(response, errorResponse);
+    }
+
+    private void writeResponse(HttpServletResponse response, ErrorResponse errorResponse) throws IOException {
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        response.flushBuffer();
+    }
+}
--- src/main/java/com/internhub/backend/config/SecurityConfig.java
+++ src/main/java/com/internhub/backend/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
 
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwtConfigure -> jwtConfigure.decoder(customJwtDecoder))
-//                .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
         );
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
