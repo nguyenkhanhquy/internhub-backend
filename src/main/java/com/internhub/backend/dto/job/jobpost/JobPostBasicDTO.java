77af63996cc002e5138c50db10fb52d6378442cf
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-11-05 20:19:12 +0700

    update: Job Post Basic DTO

--- src/main/java/com/internhub/backend/dto/job/jobpost/JobPostBasicDTO.java
+++ src/main/java/com/internhub/backend/dto/job/jobpost/JobPostBasicDTO.java
@@ -2,6 +2,7 @@ package com.internhub.backend.dto.job.jobpost;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.internhub.backend.dto.business.company.CompanyBasicDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,10 @@ public class JobPostBasicDTO {
     private String id;
 
     private String title;
+
+    private CompanyBasicDTO company;
+
+    private String type;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
     private Date createdDate;
