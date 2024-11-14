b0e4a20e79560bcbb23f1bb8bd6dd1600491209c
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-11-14 17:19:14 +0700

    update: Job Saved Controller

--- src/main/java/com/internhub/backend/controller/JobSavedController.java
+++ src/main/java/com/internhub/backend/controller/JobSavedController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/jobs/save")
+@RequestMapping("/jobs/saved")
 public class JobSavedController {
 
     private final JobSavedService jobSavedService;
