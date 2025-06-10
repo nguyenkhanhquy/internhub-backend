a5392c25a8f9baf58c627a486f9614043e808f36
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2025-06-10 09:44:49 +0700

    update: AcademicUtils

--- src/main/java/com/internhub/backend/util/AuthUtils.java
+++ src/main/java/com/internhub/backend/util/AuthUtils.java
@@ -6,10 +6,10 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class AuthUtils {
+public final class AuthUtils {
 
     private AuthUtils() {
-        throw new IllegalStateException("Utility class");
+        throw new UnsupportedOperationException("Utility class");
     }
 
     public static Authentication getAuthenticatedUser() {
